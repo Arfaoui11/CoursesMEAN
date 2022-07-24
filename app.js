1a90a9ff142f6f65f59ef4e8501b2c62bf5eeb33
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-07-24 17:02:24 +0100

    u

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const nodemailer = require('nodemailer');
 
 
 
-
-
 //const swaggerUi = require('swagger-ui-express'),
 //swaggerDocument = require('./swagger.json');
 
@@ -50,15 +48,17 @@ let userList = new Map();
 
 io.on('connection', (socket) => {
 
-    console.log('char app connected')
+    console.log('chat app connected')
     let userName = socket.handshake.query.userName;
     addUser(userName, socket.id);
+
+    let user  = userName.split(',');
 
     socket.broadcast.emit('user-list', [...userList.keys()]);
     socket.emit('user-list', [...userList.keys()]);
 
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
+        socket.broadcast.emit('message-broadcast', {message: msg, userName: user});
     })
 
     socket.on('disconnect', (reason) => {
@@ -68,8 +68,11 @@ io.on('connection', (socket) => {
 });
 
 function addUser(userName, id) {
+
+    let user  = userName.split(',');
+
     if (!userList.has(userName)) {
-        userList.set(userName, new Set(id));
+        userList.set({"userName":user[0],"image": user[1]}, new Set(id));
     } else {
         userList.get(userName).add(id);
     }
@@ -176,3 +179,113 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => {
         console.log(err)
     })
+
+
+
+
+////////////////////////////////////////
+// maybe replace with socket.io
+const WebSocket = require("ws").Server;
+const { v4: getID } = require("uuid");
+const port = 2233 ;
+
+const CLIENTS = new Map();
+
+const ROOMS = new Map();
+
+const wss = new WebSocket({ port });
+console.log("ws:// listening on %d", port);
+
+wss.on("connection", (client) => {
+    const id = getID();
+    client.uid = id;
+    CLIENTS.set(id, client);
+    const msg = {
+        type: "connection",
+        message: "Welcome",
+        id: id,
+    };
+    client.send(JSON.stringify(msg));
+
+    client.on("close", () => {
+        const leave_id = client.uid;
+        const room_id = client.room;
+        if (client.room) {
+            const thisRoom = ROOMS.get(room_id);
+            if (thisRoom.attendees.has(leave_id)) {
+                const msg = {
+                    type: "leave",
+                    message: "Tschö mit Ö",
+                    id: client.uid,
+                };
+                thisRoom.attendees.forEach((c) => {
+                    if (c !== client) {
+                        c.send(JSON.stringify(msg));
+                    }
+                });
+                thisRoom.attendees.delete(leave_id);
+                if (thisRoom.attendees.size === 0) {
+                    ROOMS.delete(room_id);
+                }
+            }
+        }
+        CLIENTS.delete(leave_id);
+    });
+    client.on("message", (m) => {
+        const msg = JSON.parse(m);
+        const room = msg.id;
+        switch (msg.type) {
+            case "connection":
+                client.name = msg.message;
+                break;
+            case "message":
+                if (ROOMS.has(room)) {
+                    const thisRoom = ROOMS.get(room);
+                    console.log(client.name);
+                    msg.id = client.name;
+                    thisRoom.attendees.forEach((c) => {
+                        c.send(JSON.stringify(msg));
+                    });
+                }
+                break;
+            case "join":
+                client.room = room;
+                if (!ROOMS.has(room)) {
+                    const newroom = {
+                        name: room,
+                        host: client,
+                        attendees: new Map(),
+                    };
+                    ROOMS.set(room, newroom);
+                }
+                const thisRoom = ROOMS.get(room);
+                thisRoom.attendees.set(client.uid, client);
+                const list = [];
+                thisRoom.attendees.forEach((c) => {
+                    list.push({ name: c.name, id: c.uid });
+                });
+                const m = { message: list, id: client.uid, type: "list" };
+                thisRoom.attendees.forEach((c) => {
+                    c.send(JSON.stringify(m));
+                });
+                break;
+            case "available":
+                if (ROOMS.has(room)) {
+                    const thisRoom = ROOMS.get(room);
+                    msg.id = msg.message;
+                    msg.message = client.name;
+                    thisRoom.attendees.forEach((c) => {
+                        console.log("call", c.name);
+                        if (c !== client) {
+                            c.send(JSON.stringify(msg));
+                        }
+                    });
+                }
+                break;
+        }
+    });
+});
+
+
+
+////////////////////////////////////////
